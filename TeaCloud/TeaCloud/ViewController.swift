d65857b517a1427f93e96b768f15c3e2530cd9ae
João Gabriel <joao.gbp@hotmail.com>
2017-10-03 22:46:16 -0300

    Colocando coisas no banco

--- TeaCloud/TeaCloud/ViewController.swift
+++ TeaCloud/TeaCloud/ViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 import CloudKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var teaName: UITextField!
+    @IBOutlet weak var teaContents: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        teaName.delegate = self
+        teaContents.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,16 +27,39 @@ class ViewController: UIViewController {
     }
 
     @IBAction func saveTea(_ sender: Any) {
-//        let dataBase = CKContainer.default().privateCloudDatabase
+        let manager = CKManager.init()
+        //let dataBase = CKContainer.default().privateCloudDatabase
         let alertaController = UIAlertController(title: "Salvar", message: "Deseja salvar o cha ?", preferredStyle: .alert)
         let acaoConfirmar = UIAlertAction(title: "Confirmar", style: .default) { (UIAlertAction) in
-            //
+            if (self.teaName.text != "") && (self.teaContents.text != "") {
+                let t = Tea(name: self.teaName.text!, contents: self.teaContents.text!)
+                manager.save(t: t)
+            }
         }
         let acaoCancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
         alertaController.addAction(acaoConfirmar)
         alertaController.addAction(acaoCancelar)
         // Apresentar alerta
         present(alertaController,animated:true,completion: nil)
+    }
+    
+    // MARK: - TextField Methods
+    
+    func closeKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField === self.teaName {
+            //self.closeKeyboard()
+            //return false
+            self.teaContents.becomeFirstResponder()
+        }else if textField === self.teaContents{
+            self.closeKeyboard()
+            return false
+        }
+        //self.closeKeyboard() // TEMPORAREMANETE AQUI
+        return false
     }
     
 }
